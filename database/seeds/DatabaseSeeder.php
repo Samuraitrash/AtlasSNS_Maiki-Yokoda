55f076e7684eecaeeca3ef5200f7a8fb20db1663
Maiki-Yokoda <>
2024-06-22 18:29:51 +0900

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //refrence qiita.com
+      $this->call(UserTableSeeder::class);
     }
+
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,7 @@
 
 @section('content')
 <h2>機能を実装していきましょう。</h2>
-
+<div class="container">
+  <div class="">
+</div>
 @endsection
